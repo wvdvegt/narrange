9560f97b5ae488e89fd6831d4cf0220655adbf7f
MarcStan <marcstan@live.com>
2015-10-03 12:10:53 +0200

    added all files from http://sourceforge.net/p/narrange/code/HEAD/tree/trunk/ as of 2015_10_03 (except that old sln was deleted and now is updated to VS 2015)

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NArrange")]
+[assembly: AssemblyCopyright("Copyright © 2007-2012 James Nies and NArrange contributors.")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("0.3.1")]
+[assembly: AssemblyFileVersion("0.3.1")]
